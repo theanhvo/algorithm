6de68d153c906040dacdcb966832142cde3651a9
theanh <theanhvo221@gmail.com>
2017-07-30 17:29:01 +0700

    update product.py

---
+++ produc_3_number.py
@@ -0,0 +1,12 @@
+product = []
+li = [1,2,3,4,-5]
+for  i in range(len(li)):
+    for j in range(i + 1, len(li)):
+        for k in range(j+1, len(li)):
+            product.append(li[i] * li[j] * li[k])
+print(product)
+product_result = product[0]
+for elem in product:
+    if product_result < elem:
+        product_result = elem
+print('Product Result Of 3 Number : {}'.format(product_result))
---
+++ product_two.py
@@ -0,0 +1,12 @@
+product = []
+li = [-11,4,2,2,2,-5]
+for  i in range(len(li)):
+    for j in range(i + 1, len(li)):
+        product.append(li[i] * li[j])
+# print(product)
+product_result = product[0]
+for elem in product:
+    if product_result < elem:
+        product_result = elem
+print('Product Result Of 2 Number : {}'.format(product_result))
+# product
